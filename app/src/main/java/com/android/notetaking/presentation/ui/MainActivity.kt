52bd7446a44bd0c7032c091c6a852d78da5ac79e
Bekjan <bekjan.omirzak98@gmail.com>
2022-04-25 11:07:23 +0600

    saved swipe

--- app/src/main/java/com/android/notetaking/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/android/notetaking/presentation/ui/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.android.notetaking.presentation.ui
 
+import android.annotation.SuppressLint
 import android.graphics.Color
 import android.os.Bundle
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.view.View
 import androidx.core.os.bundleOf
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
 import com.android.notetaking.R
 import com.android.notetaking.databinding.ActivityMainBinding
+import com.android.notetaking.presentation.tools.OnSwipeTouchListener
+import com.android.notetaking.presentation.tools.VLog
 import com.android.notetaking.presentation.ui.details.NoteDetailsFragment
 import dagger.android.support.DaggerAppCompatActivity
 import kotlinx.coroutines.CoroutineScope
@@ -15,13 +21,87 @@ import kotlinx.coroutines.launch
 
 class MainActivity : DaggerAppCompatActivity() {
 
+
     private val binding: ActivityMainBinding by viewBinding(ActivityMainBinding::inflate)
     val navController by lazy { findNavController(R.id.main_nav) }
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+
+        binding.rootLinearLayout.setOnTouchListener(object : View.OnTouchListener {
+
+            private val gestureDetector: GestureDetector
+
+
+            private val SWIPE_THRESHOLD = 100
+            private val SWIPE_VELOCITY_THRESHOLD = 100
+
+            init {
+                gestureDetector = GestureDetector(this@MainActivity, GestureListener())
+            }
+
+            override fun onTouch(v: View, event: MotionEvent): Boolean {
+                return gestureDetector.onTouchEvent(event)
+            }
+
+            private inner class GestureListener : GestureDetector.SimpleOnGestureListener() {
+
+
+                override fun onDown(e: MotionEvent): Boolean {
+                    return true
+                }
+
+                override fun onFling(
+                    e1: MotionEvent,
+                    e2: MotionEvent,
+                    velocityX: Float,
+                    velocityY: Float
+                ): Boolean {
+                    var result = false
+                    try {
+                        val diffY = e2.y - e1.y
+                        val diffX = e2.x - e1.x
+                        if (Math.abs(diffX) > Math.abs(diffY)) {
+                            if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+                                if (diffX > 0) {
+                                    onSwipeRight()
+                                } else {
+                                    onSwipeLeft()
+                                }
+                                result = true
+                            }
+                        } else if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
+                            if (diffY > 0) {
+                                onSwipeBottom()
+                            } else {
+                                onSwipeTop()
+                            }
+                            result = true
+                        }
+                    } catch (exception: Exception) {
+                        exception.printStackTrace()
+                    }
+
+                    return result
+                }
+
+
+            }
+
+            fun onSwipeRight() {}
+
+            fun onSwipeLeft() {}
+
+            fun onSwipeTop() {}
+
+            fun onSwipeBottom() {
+                VLog.d("SwipeBottom got called ")
+            }
+
+        })
     }
 
     fun settingUpToolBar(toolBar: androidx.appcompat.widget.Toolbar) {
